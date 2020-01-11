23d2548e846cd8759353cb15b85bf625ccc1b2c4
BrainShipCommunity <>
2020-01-11 11:29:12 +0530

    Remove legacy code in file writers

--- src/main/java/org/brainship/xeon/StreamFileWriter.java
+++ src/main/java/org/brainship/xeon/StreamFileWriter.java
@@ -12,7 +12,7 @@ public class StreamFileWriter {
 	private Vector<String> inp;
 	
 	public StreamFileWriter(String st, Vector<String> data) {
-		fname = ShieldSwitch.multiReplace(st, '\\');
+		fname = st;
 		inp = data;
 	}
 	
